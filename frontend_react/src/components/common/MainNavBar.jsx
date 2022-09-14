8e1d0d1c79777a6cf8ae7af1e639232ef5ca215e
Hanpark04 <97578460+Hanpark04@users.noreply.github.com>
2022-09-14 17:29:19 +0900

    [FE] NavBar, InfoEdit 퍼블리싱

--- frontend_react/src/components/common/MainNavBar.jsx
+++ frontend_react/src/components/common/MainNavBar.jsx
@@ -1,14 +1,37 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 // style import
 import "./MainNavBar.scss";
 import navLogo from "@images/logo/logo2.svg";
+import dummyProfile from "@images/icon/dummyIcon.png";
+import "./NavToolTip.scss";
 
 function MainNavBar() {
-  const activeClassName = active => {
-    const prefix = "left_nav__link fs-16 btn--";
-    return active ? `${prefix}active` : `${prefix}unactive`;
+  const navigate = useNavigate();
+
+  // const activeClassName = active => {
+  //   const prefix = "left_nav__link fs-16 btn--";
+  //   return active ? `${prefix}active` : `${prefix}unactive`;
+  // };
+  const logoutClick = () => {
+    navigate("/");
+  };
+  const [openTool, setOpenTool] = useState(false);
+  const openTooltip = () => {
+    setOpenTool(!openTool);
+  };
+  const moveMyReview = () => {
+    navigate("/");
+    if (openTool === true) {
+      setOpenTool(!openTool);
+    }
+  };
+  const moveEdit = () => {
+    navigate("/infoedit");
+    if (openTool === true) {
+      setOpenTool(!openTool);
+    }
   };
 
   return (
@@ -18,22 +41,49 @@ function MainNavBar() {
           <Link to="/" className="left_nav__link flex">
             <img className="logo" title="!213" alt="logoImage" src={navLogo} />
           </Link>
-          <NavLink
+          {/* <NavLink
             className={({ isActive }) => activeClassName(isActive)}
             to="/board"
           >
             향수모음
-          </NavLink>
+          </NavLink> */}
         </nav>
-        <nav className="right_nav notoReg">
-          <NavLink className="right_nav__link fs-16" to="/login">
-            로그인
-          </NavLink>
-          <NavLink className="right_nav__link fs-16" to="/join">
-            회원가입
-          </NavLink>
+        <nav className="nav notoReg">
+          <button type="button" className="nav_img fs-16" onClick={openTooltip}>
+            <img src={dummyProfile} alt="" />
+          </button>
         </nav>
       </nav>
+      {openTool && (
+        <div className="my_tool">
+          <div className="my_tool_box flex justify-center column">
+            <button
+              type="button"
+              to="/mypage/myfeed"
+              className="my_tool_info flex align-center fs-13"
+              onClick={moveMyReview}
+            >
+              내 리뷰
+            </button>
+            <button
+              type="button"
+              to="/infoedit"
+              className="my_tool_edit flex align-center fs-13"
+              onClick={moveEdit}
+            >
+              개인정보 수정
+            </button>
+            <button
+              type="button"
+              to="/myfeed"
+              className="my_tool_logout flex align-center fs-13"
+              onClick={logoutClick}
+            >
+              로그아웃
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- frontend_react/src/routers/Router.jsx
+++ frontend_react/src/routers/Router.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import MainNavBar from "@components/common/MainNavBar";
 import Home from "@screens/Home";
+import InfoEdit from "@screens/user/InfoEdit";
 
 function Router() {
   return (
     <>
       <MainNavBar />
-
       <Routes>
         {/* main */}
         <Route path="/" element={<Home />} />
+        {/* user */}
+        <Route path="/infoedit" element={<InfoEdit />} />
       </Routes>
     </>
   );
