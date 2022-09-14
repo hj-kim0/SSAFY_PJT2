b36f8b06a2340d110739f289b19dee346f8b638f
김혜진 <aa981204@naver.com>
2022-09-14 10:38:22 +0900

    feat : MVC패턴 초기설정

--- backend/src/main/java/com/perfectrum/backend/controller/SocialController.java
+++ backend/src/main/java/com/perfectrum/backend/controller/SocialController.java
@@ -1,16 +1,19 @@
 package com.perfectrum.backend.controller;
 
+import com.perfectrum.backend.service.KakaoUserService;
 import com.perfectrum.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserController {
+public class SocialController {
+
     private final UserService userService;
+    private final KakaoUserService kakaoUserService;
 
     @Autowired
-    UserController(UserService userService){
+    SocialController(UserService userService, KakaoUserService kakaoUserService){
         this.userService = userService;
+        this.kakaoUserService = kakaoUserService;
     }
-
 }
---
+++ backend/src/main/java/com/perfectrum/backend/service/impl/KakaoUserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.perfectrum.backend.service.impl;
+
+import com.perfectrum.backend.service.KakaoUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class KakaoUserServiceImpl implements KakaoUserService {
+}
