0365d68764f28fe78986ab32825b8e74442ebe34
김혜진 <aa981204@naver.com>
2022-09-15 18:22:35 +0900

    #2 feat : Service 추가

--- src/main/java/com/perfectrum/backend/service/impl/KakaoUserServiceImpl.java
+++ src/main/java/com/perfectrum/backend/service/impl/KakaoUserServiceImpl.java
@@ -1,10 +1,25 @@
 package com.perfectrum.backend.service.impl;
 
+import com.perfectrum.backend.domain.repository.UserRepository;
 import com.perfectrum.backend.service.KakaoUserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class KakaoUserServiceImpl implements KakaoUserService {
+    private UserRepository userRepository;
+
+    @Autowired
+    KakaoUserServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+    @Override
+    @Transactional
+    public String[] kakaoLogin(String authToken) {
+        return new String[0];
+    }
 }
---
+++ src/main/java/com/perfectrum/backend/service/impl/MainServiceImpl.java
@@ -0,0 +1,8 @@
+package com.perfectrum.backend.service.impl;
+
+import com.perfectrum.backend.service.MainService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MainServiceImpl implements MainService {
+}
