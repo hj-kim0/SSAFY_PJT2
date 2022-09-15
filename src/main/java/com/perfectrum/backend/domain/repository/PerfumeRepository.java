7269484ae16679559ddaf13e6648d45f4d578801
김혜진 <aa981204@naver.com>
2022-09-15 18:21:55 +0900

    #2 feat : repository 추가

---
+++ src/main/java/com/perfectrum/backend/domain/repository/PerfumeRepository.java
@@ -0,0 +1,14 @@
+package com.perfectrum.backend.domain.repository;
+
+import com.perfectrum.backend.domain.entity.PerfumeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface PerfumeRepository extends JpaRepository<PerfumeEntity, Integer> {
+
+    List<PerfumeEntity> findTop6ByGenderAndSeasonsContainsOrderByItemRatingDesc(String gender, String season);
+
+    List<PerfumeEntity> findTop6ByOrderByItemRatingDesc();
+}
