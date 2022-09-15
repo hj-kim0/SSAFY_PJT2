fe29185d463445a979b7690f64084a57815d3e43
김혜진 <aa981204@naver.com>
2022-09-15 18:23:01 +0900

    #2 feat : Controller 추가

--- src/main/java/com/perfectrum/backend/controller/UserController.java
+++ src/main/java/com/perfectrum/backend/controller/UserController.java
@@ -2,6 +2,7 @@ package com.perfectrum.backend.controller;
 
 import com.perfectrum.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
