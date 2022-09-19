64db77c9860e094ce742a3284353573b97f3cf1f
양승호 <today731@naver.com>
2022-09-19 16:39:29 +0900

    #4 feat: 중간저장

---
+++ src/main/java/com/perfectrum/backend/domain/repository/AccordClassRepository.java
@@ -0,0 +1,7 @@
+package com.perfectrum.backend.domain.repository;
+
+import com.perfectrum.backend.domain.entity.AccordClassEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccordClassRepository extends JpaRepository<AccordClassEntity, String> {
+}
--- src/main/java/com/perfectrum/backend/domain/repository/PerfumeRepository.java
+++ src/main/java/com/perfectrum/backend/domain/repository/PerfumeRepository.java
@@ -13,4 +13,6 @@ public interface PerfumeRepository extends JpaRepository<PerfumeEntity, Integer>
     List<PerfumeEntity> findTop6ByTimezoneAndSeasonsContainsOrderByItemRatingDesc(String timeZone, String season);
 
     List<PerfumeEntity> findTop20ByTimezoneAndSeasonsContainsOrderByItemRatingDesc(String timeZone, String season);
+
+    PerfumeEntity findTop1BySeasonsContainsAndGenderAndLongevityAndTimezone(String like_seasons,String like_gender,Integer like_longevity,String like_timezone);
 }
