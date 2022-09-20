bf12de83c2306a36bda95c6dda12b0e68b79e774
han <qkrgks378@gmail.com>
2022-09-21 02:18:53 +0900

    [FE] PerfumeSearch, Main 퍼블리싱

--- frontend_react/src/routers/Router.jsx
+++ frontend_react/src/routers/Router.jsx
@@ -4,6 +4,7 @@ import MainNavBar from "@components/common/MainNavBar";
 import Home from "@screens/Home";
 import InfoEdit from "@screens/user/InfoEdit";
 import UserReview from "@screens/user/UserReview";
+import PerfumeSearch from "@screens/perfume/PerfumeSearch";
 import PerfumeDetail from "@screens/perfume/PerfumeDetail";
 import TasteAnalysis from "@screens/perfume/TasteAnalysis";
 
@@ -18,6 +19,7 @@ function Router() {
         <Route path="/infoedit" element={<InfoEdit />} />
         <Route path="/userreview" element={<UserReview />} />
         {/* perfume */}
+        <Route path="/perfumesearch" element={<PerfumeSearch />} />
         <Route path="/perfumedetail" element={<PerfumeDetail />} />
         <Route path="/tasteanalysis" element={<TasteAnalysis />} />
       </Routes>
