a788a2ca2dc18b383121b3fdeabc4bb13720c43b
박한 <qkrgks378@gmail.com>
2022-09-21 11:19:14 +0900

    Fe publishing 박한

--- frontend_react/src/routers/Router.jsx
+++ frontend_react/src/routers/Router.jsx
@@ -2,15 +2,26 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import MainNavBar from "@components/common/MainNavBar";
 import Home from "@screens/Home";
+import InfoEdit from "@screens/user/InfoEdit";
+import UserReview from "@screens/user/UserReview";
+import PerfumeSearch from "@screens/perfume/PerfumeSearch";
+import PerfumeDetail from "@screens/perfume/PerfumeDetail";
+import TasteAnalysis from "@screens/perfume/TasteAnalysis";
 
 function Router() {
   return (
     <>
       <MainNavBar />
-
       <Routes>
         {/* main */}
         <Route path="/" element={<Home />} />
+        {/* user */}
+        <Route path="/infoedit" element={<InfoEdit />} />
+        <Route path="/userreview" element={<UserReview />} />
+        {/* perfume */}
+        <Route path="/perfumesearch" element={<PerfumeSearch />} />
+        <Route path="/perfumedetail" element={<PerfumeDetail />} />
+        <Route path="/tasteanalysis" element={<TasteAnalysis />} />
       </Routes>
     </>
   );
