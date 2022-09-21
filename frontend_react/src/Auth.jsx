9bc97ae25e227e49c608add9bf33da70f23586cf
김혁주 <lyricism3002@gmail.com>
2022-09-21 15:57:07 +0900

    login err

--- frontend_react/src/Auth.jsx
+++ frontend_react/src/Auth.jsx
@@ -5,13 +5,15 @@ import { useNavigate } from "react-router-dom";
 import cookies from "react-cookies";
 
 const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
-const REDIRECT_URI = 'http://j7c105.p.ssafy.io/oauth/kakao';
+const REDIRECT_URI = 'http://localhost:3000/oauth/kakao';
 
 function Auth() {
     const coder = new URL(window.location.href).searchParams.get("code");
     const navigate = useNavigate();
 
-    // 2. 토큰 받기
+    let accessToken = '';
+
+        // 2. 토큰 받기
     const getToken = async () => {
         const payload = qs.stringify({
             grant_type: "authorization_code",
@@ -20,33 +22,31 @@ function Auth() {
             code: coder,
         });
         try{
-            // console.log("카카오");
             const response = await axios.post(
                 "https://kauth.kakao.com/oauth/token", payload
             );
-            const sendToken = async () => 
+
+            accessToken = response.data.access_token;           
+            
+        }catch(err){
+            console.log("getToken error");
+        }
+    };
+
+    const sendToken = async () => 
             {
-                const accessToken = response.data.access_token;
                 const headers = {
                     'authToken' : accessToken,
                 }
-
-                // console.log(headers);
                 try{
                     const res = await axios.get(
-                        "http://j7c105.p.ssafy.io:8083/kakao",
                         headers,
-                        { withCredentials: true }
+                        "http://j7c105.p.ssafy.io:8083/kakao",
+                        
                     ); 
-
                     const acToken = res.data[`access-token`];
-                    
-                    // console.log(acToken);
-
                     const expires = new Date();
-
-                    expires.setMinutes(1);
-                    
+                    expires.setMinutes(1); 
                     cookies.save(
                         'access_token',
                         acToken,
@@ -55,62 +55,22 @@ function Auth() {
                             expires,
                         }
                     )
-
-                }catch(err){
                     navigate.replace("/");
-                    // console.error("sendToken err");
+                }catch(err){
+                    console.log("sendToken error");
                 }
             }
 
-            sendToken();
-            // console.log("백엔드");
-            navigate.replace("/");
-        }catch(err){
-            navigate.replace("/");
-            // console.error("getToken err");
-        }
-    };
-
-
-
-    // const sendToken = async () => {
-        // const accessToken = window.Kakao.Auth.getAccessToken();
-        // console.log(accessToken);
-        // const headers = {
-        //     authToken : accessToken,
-        // }
-        // try{
-        //     const response = await axios.post(
-        //         "http://http://j7c105.p.ssafy.io:8083/kakao", headers
-        //     );
-
-        //     const expires = new Date();
-
-        //     expires.setMinutes(10);
-        //     cookies.save(
-        //         'refresh_token',
-        //         response.data.refresh_token,
-        //         {
-        //             path: '/',
-        //             expires,
-        //         }
-        //     )
-            // navigate.replace("/");
-        // }catch(err){
-        //     console.error();
-        // }
-    // };
-
+            
     useEffect(() => {
         getToken();
     },[]);
 
+    useEffect(() => {
+        sendToken();
+    },[]);
+
     
-    // useEffect(() => {
-    //     sendToken();
-    // },[]);
-
-
     return null;
 };
 
