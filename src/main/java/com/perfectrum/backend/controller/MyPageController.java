e07714f7f0c9c2e99a679220502ec79a9bf42bcb
김혜진 <aa981204@naver.com>
2022-09-22 18:34:33 +0900

    #8. feat : 위시리스트/보유리스트 api 기능 구현

--- src/main/java/com/perfectrum/backend/controller/MyPageController.java
+++ src/main/java/com/perfectrum/backend/controller/MyPageController.java
@@ -1,12 +1,18 @@
 package com.perfectrum.backend.controller;
 
+import com.perfectrum.backend.dto.HaveListDto;
+import com.perfectrum.backend.dto.WishListDto;
+import com.perfectrum.backend.service.HaveListService;
+import com.perfectrum.backend.service.JwtService;
+import com.perfectrum.backend.service.WishListService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -16,18 +22,129 @@ public class MyPageController {
     private static final String timeOut = "access-token timeout";
     private static HttpStatus status = HttpStatus.NOT_FOUND;
 
+    private JwtService jwtService;
+    private WishListService wishListService;
+    private HaveListService haveListService;
 
-//    @GetMapping("/my-page/wish")
-//    public ResponseEntity<?> viewWishList(HttpServletRequest request){
-//        Map<String, Object> resultMap = new HashMap<>();
-//
-//        return new ResponseEntity<>(resultMap, status);
-//    }
-//
-//    @GetMapping("/my-page/wish")
-//    public ResponseEntity<?> viewHaveList(HttpServletRequest request){
-//        Map<String, Object> resultMap = new HashMap<>();
-//
-//        return new ResponseEntity<>(resultMap, status);
-//    }
+    @Autowired
+    MyPageController(JwtService jwtService, WishListService wishListService,
+                     HaveListService haveListService) {
+
+        this.jwtService = jwtService;
+        this.wishListService = wishListService;
+        this.haveListService = haveListService;
+    }
+
+    @GetMapping("/my-page/wish") // wishList 조회
+    public ResponseEntity<?> viewWishList(HttpServletRequest request){
+        Map<String, Object> resultMap = new HashMap<>();
+        String decodeId = checkToken(request, resultMap);
+
+        if(decodeId != null){
+            try{
+                List<WishListDto> list = wishListService.viewWishList(decodeId);
+                if(list != null){
+                    resultMap.put("wishList", list);
+                }else{
+                    resultMap.put("wishList", null);
+                }
+                resultMap.put("message", success);
+                status = HttpStatus.OK;
+            }catch (Exception e){
+                resultMap.put("message", fail);
+                status = HttpStatus.INTERNAL_SERVER_ERROR;
+            }
+        }
+
+        return new ResponseEntity<>(resultMap, status);
+    }
+
+    @PutMapping("/my-page/wish/{idx}")
+    public ResponseEntity<?> moveWishToHave(HttpServletRequest request, @PathVariable Integer idx){
+        Map<String, Object> resultMap = new HashMap<>();
+        String decodeId = checkToken(request, resultMap);
+
+        if(decodeId != null){
+            try{
+                wishListService.moveWishToHave(decodeId, idx);
+                resultMap.put("message", success);
+                status = HttpStatus.OK;
+            }catch (Exception e){
+                resultMap.put("message", fail);
+                status = HttpStatus.INTERNAL_SERVER_ERROR;
+            }
+        }
+        return new ResponseEntity<>(resultMap, status);
+    }
+
+    @PutMapping("/my-page/wish/delete/{idx}")
+    public ResponseEntity<?> deleteWishList(HttpServletRequest request, @PathVariable Integer idx){
+        Map<String, Object> resultMap = new HashMap<>();
+        String decodeId = checkToken(request, resultMap);
+
+        if(decodeId != null){
+            try{
+                wishListService.deleteWishList(decodeId, idx);
+                resultMap.put("message", success);
+                status = HttpStatus.OK;
+            }catch (Exception e){
+                resultMap.put("message", fail);
+                status = HttpStatus.INTERNAL_SERVER_ERROR;
+            }
+        }
+        return new ResponseEntity<>(resultMap, status);
+    }
+    @GetMapping("/my-page/have")
+    public ResponseEntity<?> viewHaveList(HttpServletRequest request){
+        Map<String, Object> resultMap = new HashMap<>();
+        String decodeId = checkToken(request, resultMap);
+
+        if(decodeId != null){
+            try{
+                List<HaveListDto> list = haveListService.viewHaveList(decodeId);
+                if(list != null){
+                    resultMap.put("haveList", list);
+                }else{
+                    resultMap.put("haveList", null);
+                }
+                resultMap.put("message", success);
+                status = HttpStatus.OK;
+            }catch (Exception e){
+                resultMap.put("message", fail);
+                status = HttpStatus.INTERNAL_SERVER_ERROR;
+            }
+        }
+        return new ResponseEntity<>(resultMap, status);
+    }
+
+    @PutMapping("/my-page/have/delete/{idx}")
+    public ResponseEntity<?> deleteHaveList(HttpServletRequest request, @PathVariable Integer idx){
+        Map<String, Object> resultMap = new HashMap<>();
+        String decodeId = checkToken(request, resultMap);
+
+        if(decodeId != null){
+            try{
+                haveListService.deleteHaveList(decodeId, idx);
+                resultMap.put("message", success);
+                status = HttpStatus.OK;
+            }catch (Exception e){
+                resultMap.put("message", fail);
+                status = HttpStatus.INTERNAL_SERVER_ERROR;
+            }
+        }
+        return new ResponseEntity<>(resultMap, status);
+    }
+
+    @GetMapping("/")
+    public String checkToken(HttpServletRequest request, Map<String, Object> resultMap){
+        String accessToken = request.getHeader("Authorization");
+        String decodeId = jwtService.decodeToken(accessToken);
+        if(!decodeId.equals("timeout")){
+            return decodeId;
+        }else{
+            resultMap.put("message", timeOut);
+            status = HttpStatus.UNAUTHORIZED;
+            return null;
+        }
+    }
 }
