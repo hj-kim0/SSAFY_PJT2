a463a8c00ec04997751b5725d07c4712ba5fb310
김혜진 <aa981204@naver.com>
2022-09-22 18:33:40 +0900

    #8. 보유리스트 조회/삭제 api 구현

--- src/main/java/com/perfectrum/backend/domain/repository/HaveListRepository.java
+++ src/main/java/com/perfectrum/backend/domain/repository/HaveListRepository.java
@@ -1,10 +1,17 @@
 package com.perfectrum.backend.domain.repository;
 
 import com.perfectrum.backend.domain.entity.HaveListEntity;
+import com.perfectrum.backend.domain.entity.UserEntity;
 import com.perfectrum.backend.domain.entity.WishListEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface HaveListRepository extends JpaRepository<HaveListEntity,Integer> {
 
 
+    List<HaveListEntity> findByUser(UserEntity userEntity);
+
+    Optional<HaveListEntity> findByUserAndIdx(Optional<UserEntity> userEntityOptional, Integer idx);
 }
