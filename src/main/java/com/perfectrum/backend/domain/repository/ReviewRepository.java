0e2d21a04258942704fa2d8dfe53c138e297ae48
양승호 <today731@naver.com>
2022-09-22 17:20:52 +0900

    #7 feat: 리뷰목록 반환 함수 추가

--- src/main/java/com/perfectrum/backend/domain/repository/ReviewRepository.java
+++ src/main/java/com/perfectrum/backend/domain/repository/ReviewRepository.java
@@ -5,8 +5,11 @@ import com.perfectrum.backend.domain.entity.UserEntity;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Integer> {
@@ -28,4 +31,13 @@ public interface ReviewRepository extends JpaRepository<ReviewEntity, Integer> {
             "WHERE r.user = :userEntity AND ((r.idx < :lastIdx AND r.totalScore = :lastScore) OR r.totalScore < :lastScore)" +
             "ORDER BY r.totalScore DESC, r.idx DESC")
     Slice<ReviewEntity> findByUserOrderByTotalScoreDescIdxDesc(UserEntity userEntity, Integer lastScore, Integer lastIdx, Pageable pageable);
+
+    ReviewEntity findByIdx(Integer reviewIdx);
+
+    List<ReviewEntity> findByPerfumeIdx(Integer perfumeIdx);
+//    @Modifying
+//    @Query("UPDATE reviews r SET r.review_img =:reviewImg, r.total_score = :totalScore,r.content = :content, r.update_time = :updateTime" +
+//            "WHERE r.idx = :idx")
+//    Integer updateReview(@Param(value="reviewImg")String reviewImg, @Param(value="totalScore")Integer total_score, @Param(value="content")String content, @Param(value="updateTime")LocalDateTime update_time);
+
 }
