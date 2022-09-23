9926ca3bc00af7ac1d6da3193688a261082f6a29
4d656f77 <4d656f77@gmail.com>
2022-09-23 10:13:35 +0900

    del: backend dir

--- backend/src/main/java/com/perfectrum/backend/domain/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.perfectrum.backend.domain.repository;
-
-import com.perfectrum.backend.domain.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, String> {
-}
--- backend/src/main/java/com/perfectrum/backend/service/impl/UserServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.perfectrum.backend.service.impl;
-
-import com.perfectrum.backend.service.UserService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserServiceImpl implements UserService {
-}
