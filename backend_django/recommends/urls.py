088b7da41ee4c602bb8bae9a024088e0305bb422
kiki249 <kiki249049@gmail.com>
2022-09-23 17:56:25 +0900

    fix : url

--- backend_django/recommends/urls.py
+++ backend_django/recommends/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('recommend/', views.collaboration), # api/recommend
-    path('recommend2/', views.collaboration2),
-    path('recommend3/', views.collaboration3),
+    path('recommend-svd/', views.collaboration), # api/recommend
+    path('recommend-cos/', views.collaboration2),
+    path('recommend-nmf/', views.collaboration3),
 ]
 
