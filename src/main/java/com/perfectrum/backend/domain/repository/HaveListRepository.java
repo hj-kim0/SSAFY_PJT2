24b96b847dc5a2d91ed96655f06821fb9217f896
양승호 <today731@naver.com>
2022-09-23 14:20:34 +0900

    #5 feat: Have리스트 개수 확인 함수 작성

--- src/main/java/com/perfectrum/backend/domain/repository/HaveListRepository.java
+++ src/main/java/com/perfectrum/backend/domain/repository/HaveListRepository.java
@@ -8,10 +8,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
+
 public interface HaveListRepository extends JpaRepository<HaveListEntity,Integer> {
 
+    HaveListEntity findByIdx(Integer idx);
 
     List<HaveListEntity> findByUser(UserEntity userEntity);
 
     Optional<HaveListEntity> findByUserAndIdx(Optional<UserEntity> userEntityOptional, Integer idx);
+
+    Long countByuserIdx(Integer userIdx);
 }
