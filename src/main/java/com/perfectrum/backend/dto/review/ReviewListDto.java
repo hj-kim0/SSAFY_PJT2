96a3ebe51d2da9a294e6cd4f54c6aa1dd8c095d6
양승호 <today731@naver.com>
2022-09-24 09:02:12 +0900

    #5 fix: 향수 리뷰 페이지네이션 처리용 Dto 작성

---
+++ src/main/java/com/perfectrum/backend/dto/review/ReviewListDto.java
@@ -0,0 +1,18 @@
+package com.perfectrum.backend.dto.review;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReviewListDto {
+        private String type;
+        private Integer lastIdx;
+        private Integer pageSize;
+
+}
