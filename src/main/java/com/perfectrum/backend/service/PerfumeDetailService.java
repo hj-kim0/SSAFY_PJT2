116d5a7987ee64586320908f2b5e49046f86d140
양승호 <today731@naver.com>
2022-09-24 09:35:05 +0900

    #5 향수상세정보 리뷰 페이지네이션 추가 구현

--- src/main/java/com/perfectrum/backend/service/PerfumeDetailService.java
+++ src/main/java/com/perfectrum/backend/service/PerfumeDetailService.java
@@ -1,13 +1,14 @@
 package com.perfectrum.backend.service;
 
 import com.perfectrum.backend.domain.entity.PerfumeEntity;
+import com.perfectrum.backend.dto.review.ReviewListDto;
 import com.perfectrum.backend.dto.review.ReviewViewDto;
 
 import java.util.Map;
 
 public interface PerfumeDetailService {
 
-    Map<String,Object> getPerfumeDetail(String decodeId, Integer perfumeIdx);
+    Map<String,Object> getPerfumeDetail(String decodeId, Integer perfumeIdx, ReviewListDto reviewListDto);
 
     Map<String,Object> addWishList(String decodeId, Integer perfumeIdx);
 
