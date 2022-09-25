c00ae917e472d6ba685acb513ec89bfa6ea82860
han <qkrgks378@gmail.com>
2022-09-26 04:05:44 +0900

    [FE]라우터_수정

--- frontend_react/src/routers/Router.jsx
+++ frontend_react/src/routers/Router.jsx
@@ -3,10 +3,15 @@ import { Route, Routes } from "react-router-dom";
 
 import MainNavBar from "@components/common/MainNavBar";
 import Home from "@screens/Home";
+import InfoEdit from "@screens/user/InfoEdit";
+import UserReview from "@screens/user/UserReview";
+import PerfumeSearch from "@screens/perfume/PerfumeSearch";
+import PerfumeDetail from "@screens/perfume/PerfumeDetail";
+import TasteAnalysis from "@screens/perfume/TasteAnalysis";
 import Login from "@screens/Login";
 import Auth from "../Auth";
 import Token from "../Token";
-import Footer from "@components/common/Footer";
+import Footer from "../components/common/Footer";
 import PersonalPerfume from "../screens/PersonalPerfume";
 
 function Router() {
@@ -15,6 +20,13 @@ function Router() {
       <MainNavBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        {/* user */}
+        <Route path="/infoedit" element={<InfoEdit />} />
+        <Route path="/userreview" element={<UserReview />} />
+        {/* perfume */}
+        <Route path="/perfumesearch" element={<PerfumeSearch />} />
+        <Route path="/detail/:id" element={<PerfumeDetail />} />
+        <Route path="/tasteanalysis" element={<TasteAnalysis />} />
         <Route path="/login" element={<Login/>}/>
         <Route path="/oauth/kakao" element={<Auth/>} />
         <Route path="/oauth/token" element={<Token/>}/>
