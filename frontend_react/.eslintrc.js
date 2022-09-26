14c1e8989e1cea8aed4d65bedfe8ed60e2912d03
Hanpark04 <97578460+Hanpark04@users.noreply.github.com>
2022-09-26 17:42:08 +0900

    [FE] TasteInfo scss

--- frontend_react/.eslintrc.js
+++ frontend_react/.eslintrc.js
@@ -21,28 +21,27 @@ module.exports = {
     "no-param-reassign": [
       "error",
       { props: true, ignorePropertyModificationsFor: ["state"] }
-    ]
-    ,"import/no-unresolved" : 0
-    ,"arrow-body-style": "off"
-    ,"object-shorthand": "off"
-    ,"no-console": "off"
-    ,"react/prop-types": "off"
-    ,"react/self-closing-comp": "off"
-    ,"react/function-component-definition": "off"
-    ,"react/destructuring-assignment": "off"
-    ,"react/no-array-index-key": "off"
-    ,"global-require": "off"
-    ,"no-plusplus": "off"
-    ,"prefer-template": "off"
-    ,"jsx-a11y/alt-text": "off"
-    ,"default-param-last": "off"
-    ,"no-unreachable": "off"
-    ,"no-unused-vars": "off"
-    ,"operator-assignment": "off"
-    ,"import/prefer-default-export": "off"
-    ,"no-lone-blocks": "off"
-    ,"react/jsx-no-useless-fragment": "off"
-    ,
+    ],
+    "import/no-unresolved": 0,
+    "arrow-body-style": "off",
+    "object-shorthand": "off",
+    "no-console": "off",
+    "react/prop-types": "off",
+    "react/self-closing-comp": "off",
+    "react/function-component-definition": "off",
+    "react/destructuring-assignment": "off",
+    "react/no-array-index-key": "off",
+    "global-require": "off",
+    "no-plusplus": "off",
+    "prefer-template": "off",
+    "jsx-a11y/alt-text": "off",
+    "default-param-last": "off",
+    "no-unreachable": "off",
+    "no-unused-vars": "off",
+    "operator-assignment": "off",
+    "import/prefer-default-export": "off",
+    "no-lone-blocks": "off",
+    "react/jsx-no-useless-fragment": "off"
   },
   settings: {
     "import/resolver": {
@@ -51,7 +50,7 @@ module.exports = {
           // ["@components", "./src/components"],
           // ["@screens", "./src/screens"],
           ["@styles", "./src/assets/styles"],
-          ["@images", "./src/assets/images"],
+          ["@images", "./src/assets/images"]
           // ["@utils", "./src/utils"]
         ],
         extensions: [".ts", ".js", ".jsx", ".json", ".svg"]
