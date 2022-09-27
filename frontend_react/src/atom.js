1475bcbc226a85bac77df5b119b530006cea268f
4d656f77 <4d656f77@gmail.com>
2022-09-27 14:37:24 +0900

    feat: 프로필 변경 구현

--- frontend_react/src/atom.js
+++ frontend_react/src/atom.js
@@ -24,3 +24,17 @@ export const userState = atom({
     },
     effects_UNSTABLE: [persistAtom],
 });
+
+export const userProfileState = atom({
+    key: "userProfileState",
+    default: {
+        "idx": -1,
+        "userId": "",
+        "profileImg": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAIUAAABpCAIAAACbPneSAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAD8SURBVHhe7dFBDQAwCACx2UPnEIqNPi6pg74/G0cflj4sfVj6sPRh6cPSh6UPSx+WPix9WPqw9GHpw9KHpQ9LH5Y+LH1Y+rD0YenD0oelD0sflj4sfVj6sPRh6cPSh6UPSx+WPix9WPqw9GHpw9KHpQ9LH5Y+LH1Y+rD0YenD0oelD0sflj4sfVj6sPRh6cPSh6UPSx+WPix9WPqw9GHpw9KHpQ9LH5Y+LH1Y+rD0YenD0oelD0sflj4sfVj6sPRh6cPSh6UPSx+WPix9WPqw9GHpw9KHpQ9LH5Y+LH1Y+rD0YenD0oelD0sflj4sfVj6sPRh6cPSh6UPyewBt75BTHRWB6IAAAAASUVORK5CYII=", 
+        "nickname": "닉네임",
+        "gender": "성별",
+        "seasons": "시즌",
+        "accordClass": "클래스"
+    },
+    effects_UNSTABLE: [persistAtom],
+});
