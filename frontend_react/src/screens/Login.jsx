4fc8d2e2ef4dbc1e1b9e4d11f5f61cdb6e6903c9
4d656f77 <4d656f77@gmail.com>
2022-09-29 10:40:34 +0900

    fix: Server URI

--- frontend_react/src/screens/Login.jsx
+++ frontend_react/src/screens/Login.jsx
@@ -3,8 +3,8 @@ import React from "react";
 import KakaoLogin from "../assets/images/login/kakao_login_medium_narrow.png";
 
 const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
-// const REDIRECT_URI = "http://j7c105.p.ssafy.io/oauth/kakao";
-const REDIRECT_URI = 'http://localhost:3000/oauth/kakao';
+const REDIRECT_URI = "http://j7c105.p.ssafy.io/oauth/kakao";
+// const REDIRECT_URI = 'http://localhost:3000/oauth/kakao';
 
 const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
