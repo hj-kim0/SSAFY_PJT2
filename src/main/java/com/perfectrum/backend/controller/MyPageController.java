1ffc2827a92b2d9920027a16e5fa0a6cf5beba9a
김혜진 <aa981204@naver.com>
2022-09-30 18:20:46 +0900

    #9. fix : 리뷰 수정 api 수정

--- src/main/java/com/perfectrum/backend/controller/MyPageController.java
+++ src/main/java/com/perfectrum/backend/controller/MyPageController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -92,7 +93,7 @@ public class MyPageController {
                 if(list != null){
                     resultMap.put("wishList", list);
                 }else{
-                    resultMap.put("wishList", null);
+                    resultMap.put("wishList", new ArrayList<>());
                 }
                 resultMap.put("message", success);
                 status = HttpStatus.OK;
@@ -151,7 +152,7 @@ public class MyPageController {
                 if(list != null){
                     resultMap.put("haveList", list);
                 }else{
-                    resultMap.put("haveList", null);
+                    resultMap.put("haveList", new ArrayList<>());
                 }
                 resultMap.put("message", success);
                 status = HttpStatus.OK;
