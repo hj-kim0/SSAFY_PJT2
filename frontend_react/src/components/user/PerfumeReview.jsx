6cb48b611c94c4d258a353802054f3842bb00ae4
han <qkrgks378@gmail.com>
2022-10-04 16:47:04 +0900

    [FE] PerfumeReview Delete 구현

--- frontend_react/src/components/user/PerfumeReview.jsx
+++ frontend_react/src/components/user/PerfumeReview.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import axios from "axios";
 import { useRecoilValue } from 'recoil';
-import { userProfileState } from "../../atom";
+import { userProfileState, userState } from "../../atom";
 import dummyProfile from "@images/icon/dummyIcon.png";
 // import starRating from "@images/icon/star.svg";
 import ystar from "@images/icon/ystar.png";
@@ -12,24 +14,49 @@ import "./PerfumeReview.scss";
 
 function PerfumeReview(props) {
   const userProfile = useRecoilValue(userProfileState);
+  const userLoginState = useRecoilValue(userState);
+  const { id } = useParams();
+  const [editInfo, setEditInfo] = useState(false);
+  const navigate = useNavigate();
+  const move = () => {
+    navigate(`/detail/${id}`);
+    window.location.reload();
+  };
   const perfumeItem = props.item;
-  const starList = []
+  const starList = [];
   let i = 0;
   for (; i < perfumeItem.totalScore; ++i) {
     starList.push(<span><img className="perfumeReview_profile_rating_ystar" src={ystar} alt="" /></span>)
-  }
-  const longList = []
+  };
+  const longList = [];
   let j = 0;
   for (; j < perfumeItem.longevity; ++j) {
     longList.push(<span><img className="perfumeReview_info_rating_long_emo" src={long} alt="" /></span>)
-  }
-  const sillageList = []
+  };
+  const sillageList = [];
   let k = 0;
   for (; k < perfumeItem.sillageScore; ++k) {
     sillageList.push(<span><img className="perfumeReview_info_rating_sil_emo" src={sillage} alt="" /></span>)
-  }
-  console.log(perfumeItem.userNickname);
-  console.log(userProfile[0].nickname);
+  };
+  const deleteReview = () => {
+    if (window.confirm("리뷰를 삭제하시겠습니까?")) {
+      axios({
+        method : "put",
+        url : `http://j7c105.p.ssafy.io:8083/detail/${id}/review/delete/${perfumeItem.idx}`,
+        headers : {
+          Authorization : userLoginState.sToken
+        }
+      })
+      .then(res => console.log(res))
+      .catch(error => console.log(error))
+      move();
+    }
+  };
+  const editReview = () => {
+    
+  };
+  console.log(perfumeItem);
+  // console.log(userProfile[0].nickname);
   return (
     <div id="perfumeReview" className="perfumeReview flex">
       <div className="perfumeReview_profile flex">
@@ -75,7 +102,7 @@ function PerfumeReview(props) {
         <button className="perfumeReview_btns_edit" type="button">
           <EditIcon sx={{ fontSize: 36, color: "black"}} />
         </button>
-        <button className="perfumeReview_btns_del" type="button">
+        <button className="perfumeReview_btns_del" type="button" onClick={deleteReview}>
           <DeleteIcon sx={{ fontSize: 36, color: "black"}} />
         </button>
       </div>
